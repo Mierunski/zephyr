d362084587c8934ade889638456714f0ed0eaec3
Mieszko Mierunski <mieszko.mierunski@nordicsemi.no>
2019-05-24 16:46:27 +0200

    example: samples: Add Simple led driver usage to hello world
    Signed-off-by: Mieszko Mierunski <mieszko.mierunski@nordicsemi.no>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,8 +6,16 @@
 
 #include <zephyr.h>
 #include <misc/printk.h>
+#include <simple_led.h>
 
 void main(void)
 {
 	printk("Hello World! %s\n", CONFIG_BOARD);
+	struct device *led = device_get_binding(DT_NORDIC_SIMPLE_LED_0_LABEL);
+	while(1) {
+		simple_led_on(led);
+		k_sleep(100);
+		simple_led_off(led);
+		k_sleep(100);
+	}
 }
